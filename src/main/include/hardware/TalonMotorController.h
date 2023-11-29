9ecb15356e30f694f8e451e860ea8b0e17ed5b9a
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-11-29 14:39:45 -0600

    talonmotorcontroller

--- src/main/include/hardware/TalonMotorController.h
+++ src/main/include/hardware/TalonMotorController.h
@@ -14,12 +14,12 @@ public:
         TalonSRX
     };
 
-    double encoderTicks[2];
+    double encoderTicksPerRadian[2];
 
     TalonMotorController(int deviceId, TalonModel model) : innerTalon(deviceId, model == TalonFX ? "Talon FX" : "Talon SRX"), model(model) 
     {
-        encoderTicks[TalonFX] = 2048 / M_PI / 2;
-        encoderTicks[TalonSRX] = 4096 / M_PI / 2;
+        encoderTicksPerRadian[TalonFX] = 2048 / M_PI / 2;
+        encoderTicksPerRadian[TalonSRX] = 4096 / M_PI / 2;
         switch (model) {
             case (TalonFX):
                 innerTalon.Config_IntegralZone(0, 0);
@@ -50,13 +50,13 @@ public:
     {
         innerTalon.Set(
             ctre::phoenix::motorcontrol::ControlMode::Velocity,
-            targetAngularVelocity * encoderTicks[model] / 10
+            targetAngularVelocity * encoderTicksPerRadian[model] / 10
         );
     }
 
     double GetAngularVelocity() override
     {
-        return innerTalon.GetSelectedSensorVelocity() / encoderTicks[model] * 10;
+        return innerTalon.GetSelectedSensorVelocity() / encoderTicksPerRadian[model] * 10;
     }
 
     void SetAngle(frc::Rotation2d targetRotation) override
@@ -64,9 +64,92 @@ public:
         switch(model) {
             case TalonFX:
                 innerTalon.Set(
-                    ctre::phoenix::motorcontrol::
+                    ctre::phoenix::motorcontrol::ControlMode::MotionMagic,
+                    (targetRotation.Radians() * encoderTicksPerRadian[model]).value()
                 );
+                break;
+            case TalonSRX:
+                innerTalon.Set(
+                    ctre::phoenix::motorcontrol::ControlMode::Position,
+                    (targetRotation.Radians() * encoderTicksPerRadian[model]).value()
+                );
+                break;
         }
+    }
+
+    double GetAngle() override
+    {
+        return innerTalon.GetSelectedSensorPosition() / encoderTicksPerRadian[model];
+    }
+
+    EncodedMotorController *SetCurrentLimit(int currentLimit) override
+    {
+        innerTalon.ConfigSupplyCurrentLimit(
+            SupplyCurrentLimitConfiguration(
+                true,
+                currentLimit,
+                currentLimit + 1,
+                0.1
+            ),
+            50
+        );
+        return this;
+    }
+
+    EncodedMotorController *SetPid(double p, double i, double d) override
+    {
+        innerTalon.Config_kP(0, p);
+        innerTalon.Config_kI(0, i);
+        innerTalon.Config_kD(0, d);
+        return this;
+    }
+
+    EncodedMotorController *SetMinAngle(double minPosition) override
+    {
+        innerTalon.ConfigReverseSoftLimitEnable(true);
+        innerTalon.ConfigReverseSoftLimitThreshold(minPosition * encoderTicksPerRadian[model]);
+        return this;
+    }
+
+    EncodedMotorController *SetMaxAngle(double maxPosition) override
+    {
+        innerTalon.ConfigForwardSoftLimitEnable(true);
+        innerTalon.ConfigForwardSoftLimitThreshold(maxPosition * encoderTicksPerRadian[model]);
+        return this;
+    }
+
+    EncodedMotorController *SetMinOutput(double minOutput) override
+    {
+        innerTalon.ConfigPeakOutputReverse(minOutput);
+        return this;
+    }
+
+    EncodedMotorController *SetMaxOutput(double maxOutput) override
+    {
+        innerTalon.ConfigPeakOutputForward(maxOutput);
+        return this;
+    }
+
+    EncodedMotorController *SetInversion(bool shouldInvert) override
+    {
+        innerTalon.SetInverted(shouldInvert);
+        return this;
+    }
+
+    EncodedMotorController *SetBrakeOnIdle(bool shouldBrake) override
+    {
+        innerTalon.SetNeutralMode(
+            shouldBrake
+            ? NeutralMode::Brake
+            : NeutralMode::Coast
+        );
+        return this;
+    }
+
+    EncodedMotorController *SetAngleTolerance(double tolerance) override
+    {
+        innerTalon.ConfigAllowableClosedloopError(0, tolerance * encoderTicksPerRadian[model]);
+        return this;
     }
 
 private:
