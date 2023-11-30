be56f189721bb7cc62f87a4a8695d7c6db248ee5
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-11-29 18:03:16 -0600

    limelight complete

--- src/main/include/hardware/Limelight.h
+++ src/main/include/hardware/Limelight.h
@@ -3,6 +3,7 @@
 
 #include <frc/geometry/Pose2d.h>
 #include <networktables/NetworkTable.h>
+#include <frc/DriverStation.h>
 #include "networktables/NetworkTableInstance.inc"
 
 class Limelight
@@ -14,12 +15,12 @@ public:
         table = nt::NetworkTableInstance::GetDefault().GetTable(limelightName);
     }
 
-    void setPipeline(int index)
+    void SetPipeline(int index)
     {
         setEntry("pipeline", index);
     }
 
-    frc::Pose2d getRobotPoseToField()
+    frc::Pose2d GetRobotPoseToField()
     {
         std::vector<double> raw = table.get()->GetEntry("botpose").GetDoubleArray(def);
         return frc::Pose2d(
@@ -29,6 +30,72 @@ public:
         );
     }
 
+    frc::Pose2d GetRobotPoseToAlliance(frc::DriverStation::Alliance alliance)
+    {
+        std::vector<double> raw;
+        switch(alliance)
+        {
+            case frc::DriverStation::Alliance::kRed:
+                raw = table.get()->GetEntry("botpose_wpired").GetDoubleArray(def);
+                break;
+            case frc::DriverStation::Alliance::kBlue:
+                raw = table.get()->GetEntry("botpose_wpiblue").GetDoubleArray(def);
+                break;
+            default:
+                break;
+        }
+        return frc::Pose2d(
+            units::meter_t{raw[0]},
+            units::meter_t{raw[1]},
+            frc::Rotation2d(units::degree_t{raw[5]})
+        );
+    }
+
+    frc::Pose2d GetRobotPoseToTarget()
+    {
+        std::vector<double> raw = table.get()->GetEntry("botpose_targetspace").GetDoubleArray(def);
+        return frc::Pose2d(
+            units::meter_t{raw[0]},
+            units::meter_t{raw[1]},
+            frc::Rotation2d(units::degree_t{raw[5]})
+        );
+    }
+
+    frc::Pose2d GetTargetPoseToCamera()
+    {
+        std::vector<double> raw = table.get()->GetEntry("targetpose_cameraspace").GetDoubleArray(def);
+        return frc::Pose2d(
+            units::meter_t{raw[0]},
+            units::meter_t{raw[1]},
+            frc::Rotation2d(units::degree_t{raw[5]})
+        );
+    }
+
+    frc::Pose2d GetTargetPoseToRobot()
+    {
+        std::vector<double> raw = table.get()->GetEntry("targetpose_robotspace").GetDoubleArray(def);
+        return frc::Pose2d(
+            units::meter_t{raw[0]},
+            units::meter_t{raw[1]},
+            frc::Rotation2d(units::degree_t{raw[5]})
+        );
+    }
+
+    frc::Pose2d GetCameraPoseToTarget()
+    {
+        std::vector<double> raw = table.get()->GetEntry("camerapose_targetspace").GetDoubleArray(def);
+        return frc::Pose2d(
+            units::meter_t{raw[0]},
+            units::meter_t{raw[1]},
+            frc::Rotation2d(units::degree_t{raw[5]})
+        );
+    }
+
+    int GetTargetTagId()
+    {
+        return (int) table.get()->GetEntry("tid").GetInteger(0);
+    }
+    
 private:
     std::shared_ptr<nt::NetworkTable> table;
     double def[6]; // A default return value
